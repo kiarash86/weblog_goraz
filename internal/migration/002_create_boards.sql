ed03ceb28f7e273adec43fbce850eea9ea5b30d3
krsh <kiavash86habibi@gmail.com>
2026-07-28 12:41:54 +0330

    rename name to title

--- internal/migration/002_create_boards.sql
+++ internal/migration/002_create_boards.sql
@@ -2,7 +2,7 @@ CREATE TABLE
     boards (
         id SERIAL PRIMARY KEY,
         author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
-        name varchar(50)  NOT NULL DEFAULT TITLE,
+        title varchar(50)  NOT NULL DEFAULT TITLE,
         content text  NOT NULL DEFAULT TEXT,
         is_private boolean  NOT NULL DEFAULT FALSE,
         img_path varchar(100) 
--- internal/models/board.go
+++ internal/models/board.go
@@ -3,7 +3,7 @@ package models
 type Board struct {
 	ID        int    `json:"id"`
 	AuthorID  int    `json:"author_id"`
-	Name      string `json:"name"`
+	Title      string `json:"title"`
 	Content   string `json:"content"`
 	ISPrivate bool   `json:"is_private"`
 	ImagePath string `json:"img_path"`
