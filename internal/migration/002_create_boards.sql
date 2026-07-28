10645c3f20a16dd3fc70049ffb18b017b4d985af
krsh <kiavash86habibi@gmail.com>
2026-07-29 00:17:17 +0330

    add comma(forgot)

--- internal/migration/002_create_boards.sql
+++ internal/migration/002_create_boards.sql
@@ -1,7 +1,7 @@
 CREATE TABLE
     boards (
         id SERIAL PRIMARY KEY,
-        author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+        author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE ,
         title varchar(50)  NOT NULL DEFAULT TITLE,
         content text  NOT NULL DEFAULT TEXT,
         is_private boolean  NOT NULL DEFAULT FALSE,
--- internal/migration/003_create_comments.sql
+++ internal/migration/003_create_comments.sql
@@ -1,8 +1,8 @@
 CREATE TABLE
     comments (
         id SERIAL PRIMARY KEY,
-        author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
-        board_id INTEGER NOT NULL REFERENCES boards(id) ON DELETE CASCADE
+        author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        board_id INTEGER NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
 
-        content Text  NOT NULL DEFAULT "comment",
+        content Text  NOT NULL DEFAULT 'comment',
     )
