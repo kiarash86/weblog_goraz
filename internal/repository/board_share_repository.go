28ec178d844e3e2453940a7fe6d27678df3f7e41
krsh <kiavash86habibi@gmail.com>
2026-07-28 21:13:43 +0330

    add board share repo

---
+++ internal/repository/board_share_repository.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"context"
+	"weblog/internal/models"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type BoardShareRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewBoardShareRepository(pp *pgxpool.Pool) *BoardShareRepository {
+	return &BoardShareRepository{db: pp}
+}
+
+func (ur *BoardShareRepository) Delete(ctx context.Context, userID int ,  boardID int) error {
+	query := `DELETE FROM board_shares WHERE user_id = $1 AND  board_id = $2`
+	_, err := ur.db.Exec(ctx, query, userID , boardID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ur *BoardShareRepository) Add( ctx context.Context, userID int ,  boardID int) (*models.Board_share, error) {
+	query :=
+		`
+		INSERT INTO board_shares (user_id, board_id)
+		VALUES ($1, $2)
+		RETURNING user_id, board_id
+		`
+
+	var boardShare models.Board_share
+	err := ur.db.QueryRow(ctx, query,userID, boardID).Scan(
+		&boardShare.UserID, &boardShare.BoardID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &boardShare, nil
+}
+
+
+func (ur *BoardShareRepository) HasAccess(ctx context.Context, userID int ,  boardID int) (hasAccess bool ,err error) {
+	query := `SELECT EXISTS(SELECT 1 FROM board_shares WHERE user_id = $1 AND  board_id = $2)`
+	 err = ur.db.QueryRow(ctx, query, userID , boardID).Scan(&hasAccess)
+	if err != nil {
+		return
+	}
+	return
+}
