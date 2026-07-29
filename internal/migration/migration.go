2405b40a9b96eae75b5e62bb891bf1de6dfb1780
krsh <kiavash86habibi@gmail.com>
2026-07-29 18:05:37 +0330

    add automatic migration (ai)

---
+++ internal/migration/migration.go
@@ -0,0 +1,43 @@
+package migration
+
+import (
+	"embed"
+	"errors"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	pgxmigrate "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+)
+
+// ai code
+// later i will check to see what this is doing
+var migrationFiles embed.FS
+
+func Run(pool *pgxpool.Pool) error {
+	db := stdlib.OpenDBFromPool(pool)
+	defer db.Close()
+
+	driver, err := pgxmigrate.WithInstance(db, &pgxmigrate.Config{})
+	if err != nil {
+		return fmt.Errorf("migration: init driver: %w", err)
+	}
+
+	source, err := iofs.New(migrationFiles, ".")
+	if err != nil {
+		return fmt.Errorf("migration: load embedded files: %w", err)
+	}
+
+	m, err := migrate.NewWithInstance("iofs", source, "pgx5", driver)
+	if err != nil {
+		return fmt.Errorf("migration: init migrate: %w", err)
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("migration: apply: %w", err)
+	}
+
+	return nil
+}
