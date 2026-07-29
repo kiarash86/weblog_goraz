a03bc9d3eb4ba157cf12294e27e868ca49367501
krsh <kiavash86habibi@gmail.com>
2026-07-29 09:42:17 +0330

    typeo mistake fixed

--- internal/migration/002_create_boards.sql
+++ internal/migration/002_create_boards.sql
@@ -2,8 +2,8 @@ CREATE TABLE
     boards (
         id SERIAL PRIMARY KEY,
         author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE ,
-        title varchar(50)  NOT NULL DEFAULT TITLE,
-        content text  NOT NULL DEFAULT TEXT,
+        title varchar(50)  NOT NULL DEFAULT 'TITLE',
+        content text  NOT NULL DEFAULT 'TEXT',
         is_private boolean  NOT NULL DEFAULT FALSE,
         img_path varchar(100) 
     )
--- internal/migration/003_create_comments.sql
+++ internal/migration/003_create_comments.sql
@@ -3,6 +3,5 @@ CREATE TABLE
         id SERIAL PRIMARY KEY,
         author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
         board_id INTEGER NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
-
-        content Text  NOT NULL DEFAULT 'comment',
+        content Text NOT NULL DEFAULT 'comment'
     )
