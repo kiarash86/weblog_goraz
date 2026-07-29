c48de6dbe121799694f5aad53a3702d7fb2a53f9
krsh <kiavash86habibi@gmail.com>
2026-07-29 16:00:38 +0330

    add func isowner to board repo

--- internal/repository/board_repository.go
+++ internal/repository/board_repository.go
@@ -55,12 +55,23 @@ var board models.Board
 }
 
 
+func (ur * BoardRepository) IsOwner(ctx context.Context  , boardID int, userID int  ) (bool , error){
+	query := `SELECT EXISTS(SELECT 1 FROM boards WHERE id = $1 AND author_id = $2)`
+	var isOwner bool
+	err := ur.db.QueryRow(ctx , query , boardID , userID).Scan(&isOwner)
 
-func (ur *BoardRepository) ListFeed(ctx context.Context, authorID int) (boards []*models.Board,err error) {
+	if err != nil {
+		return false , err
+	}
+	return  isOwner , nil
+}
+
+
+func (ur *BoardRepository) ListFeed(ctx context.Context, userID int) (boards []*models.Board,err error) {
 	query := `SELECT id , author_id, title , content , is_private , img_path FROM boards WHERE is_private = false
    OR author_id = $1
    OR id IN (SELECT board_id FROM board_shares WHERE user_id = $1)`
-   rows , err := ur.db.Query(ctx, query,authorID )
+   rows , err := ur.db.Query(ctx, query,userID )
    
 if err != nil {
 	return nil, err
