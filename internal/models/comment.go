f1fc81dfaddfe0e482edda5963c1886aa43f5990
krsh <kiavash86habibi@gmail.com>
2026-07-30 16:45:24 +0330

    add username to comment model

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -1,8 +1,9 @@
 package models
 
 type Comment struct {
-	ID       int    `json:"id"`
-	AuthorID int    `json:"author_id"`
-	BoardID  int    `json:"board_id"`
-	Content  string `json:"content"`
+	ID             int    `json:"id"`
+	AuthorID       int    `json:"author_id"`
+	BoardID        int    `json:"board_id"`
+	Content        string `json:"content"`
+	AuthorUsername string `json:"author_username"`
 }
