389717dd0e73dfcfca39f567646f52c04721b30f
krsh <kiavash86habibi@gmail.com>
2026-07-30 18:17:55 +0330

    add username to board model

--- internal/models/board.go
+++ internal/models/board.go
@@ -1,10 +1,11 @@
 package models
 
 type Board struct {
-	ID        int    `json:"id"`
-	AuthorID  int    `json:"author_id"`
-	Title      string `json:"title"`
-	Content   string `json:"content"`
-	ISPrivate bool   `json:"is_private"`
-	ImagePath string `json:"img_path"`
+	ID             int    `json:"id"`
+	AuthorID       int    `json:"author_id"`
+	Title          string `json:"title"`
+	Content        string `json:"content"`
+	ISPrivate      bool   `json:"is_private"`
+	ImagePath      string `json:"img_path"`
+	AuthorUsername string `json:"author_username"`
 }
